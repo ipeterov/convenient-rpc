9af1b59b7d7e753092b0110c8ebad5f785972e39
Ilya Peterov <ipeterov1@gmail.com>
2016-04-19 01:26:49 +0300

    Changed API to correspond to README

--- control.py
+++ control.py
@@ -1,5 +1,4 @@
 import json
-from itertools import islice, zip_longest
 
 from flask import Flask, jsonify, request
 
@@ -10,52 +9,32 @@ app = Flask(__name__)
 manager = TaskManager()
 
 
-@app.route("/submit_task", methods=['POST'])
+@app.route("/user/submit_task", methods=['POST'])
 def submit_task():
-    argslist = json.loads(request.args.get('argslist', '[]'))
-    kwargslist = json.loads(request.args.get('kwargslist', '[]'))
-    ids = []
-    for args, kwargs in islice(zip_longest(argslist, kwargslist, fillvalue=None), max(map(len, (argslist, kwargslist)))):
-        task = {
-            'package': request.args.get('package', None),
-            'version': request.args.get('version', None),
-            'function': request.args['function'],
-            'args': args,
-            'kwargs': kwargs
-        }
-        id_ = manager.add_task(task)
-        ids.append(id_)
+    task = request.json()
+    id_ = manager.add_task(task)
 
-    return jsonify(sucsess=True, ids=ids)
+    return jsonify(sucsess=True, id=id_)
 
-@app.route("/request_answer", methods=['GET'])
-def request_answer():
-    id_ = request.args['id']
-    blocking = int(request.args.get('blocking', '1'))
+@app.route("/user/request_answer/<id_>", methods=['GET'])
+def request_answer(id_):
+    try:
+        return jsonify(sucsess=True, answer=manager.get_answer(id_))
+    except NotReadyException:
+        return jsonify(sucsess=False, reason='Task with this ID is not ready yet')
 
-    if blocking:
-        while True:
-            try:
-                answer = manager.get_answer(id_)
-                break
-            except NotReadyException:
-                pass
-        return jsonify(sucsess=True, answer=answer)
-    else:
-        try:
-            return jsonify(sucsess=True, answer=manager.get_answer(id_))
-        except NotReadyException:
-            return jsonify(sucsess=False, reason='Task with this ID is not ready yet')
-
-@app.route("/request_task", methods=['GET'])
+@app.route("/worker/request_task", methods=['GET'])
 def request_task():
     id_, task = manager.get_task()
     return jsonify(sucsess=True, id=id_, task=task)
 
-@app.route("/submit_answer", methods=['POST'])
+@app.route("/worker/submit_answer", methods=['POST'])
 def submit_answer():
-    id_ = request.values['id']
-    answer = request.values['answer']
+    r = request.json()
+
+    id_ = r['id']
+    answer = r['answer']
+
     try:
         manager.add_answer(id_, answer)
         return jsonify(sucsess=True)
