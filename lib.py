e01a3d65f56d16f1b248f7777e295e9bec134bbb
Ilya Peterov <ipeterov1@gmail.com>
2016-04-19 22:10:00 +0300

    Now using a list instead of a queue to store unsent tasks, removed all the locks because all the operations are atomic, changed `estimate_runtime` to accept task_hash instead of task itself, implemented `estimate_time_left`.

--- lib.py
+++ lib.py
@@ -1,6 +1,5 @@
 import uuid
-from threading import RLock
-from queue import Queue
+from collections import Counter
 
 class TaskManager:
 
@@ -10,64 +9,53 @@ class TaskManager:
 
     def __init__(self):
         self.tasks = {}
-        self.tasks_lock = RLock()
-
         self.answers = {}
-        self.answers_lock = RLock()
+
+        self.unsent_tasks = []
 
         self.task_performance = {}
-        self.task_performance_lock = RLock()
-
-        self.unsent_tasks = Queue()
 
     def add_task(self, task):
         id_ = str(uuid.uuid4())
-        with self.tasks_lock:
-            self.tasks[id_] = task
-            self.unsent_tasks.put(id_)
+        self.tasks[id_] = task
+        self.unsent_tasks.append(id_)
+
         return id_
 
     def get_answer(self, id_):
-        with self.answers_lock:
-            ready = id_ in self.answers
-
-        if ready:
+        if id_ in self.answers:
             return self.answers.pop(id_)
         else:
             raise NotReadyException()
 
     def get_task(self):
-        id_ = self.unsent_tasks.get()
-
-        with self.tasks_lock:
-            task = self.tasks[id_]
+        id_ = self.unsent_tasks.pop(0)
+        task = self.tasks[id_]
 
         return id_, task
 
     def add_answer(self, id_, answer, time=None):
-        with self.tasks_lock:
-            task = self.tasks[id_]
-            hash_key = self.hash_task(task)
-
-            if id_ in self.tasks:
-                del self.tasks[id_]
-            else:
-                raise WrongIDException()
-
-        with self.answers_lock:
-            self.answers[id_] = answer
-
-        if time != None:
-            with self.task_performance_lock:
-                self.task_performance.setdefault(hash_key, []).append(time)
-
-    def estimate_runtime(self, task):
+        task = self.tasks[id_]
         hash_key = self.hash_task(task)
 
-        with self.task_performance_lock:
-            if hash_key in self.task_performance:
-                times = self.task_performance[hash_key]
-                return sum(times) / len(times)
+        if id_ in self.tasks:
+            del self.tasks[id_]
+        else:
+            raise WrongIDException()
+
+        self.answers[id_] = answer
+
+        if time != None:
+            self.task_performance.setdefault(hash_key, []).append(time)
+
+    def estimate_runtime(self, hash_key):
+        if hash_key in self.task_performance:
+            times = self.task_performance[hash_key]
+            return sum(times) / len(times)
+
+    def estimate_time_left(self):
+        tasks = Counter(self.hash_task(task) for task in self.unsent_tasks).items()
+        return sum(self.estimate_runtime(hash_key) * count for hash_key, count in tasks)
 
 
 class NotReadyException(Exception):
